04ffb8805fee3388af5a707c57292c70b27b44c2
User <swigorvas@gmail.com>
2023-03-20 07:15:55 +0200

    Project, v1

---
+++ src/PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="HomeController.cs" company="SegmentationFault">
+// Copyright (c) SegmentationFault. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this._logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        this._logger.LogInformation("Index from HomeController called.");
+        return this.RedirectToRoute("classrooms-index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return this.View();
+    }
+}
---
+++ src/PresentationLayer/wwwroot/js/question-group.js
@@ -0,0 +1,57 @@
+﻿function addGroup(groupName) {
+    $.ajax({
+        type: "POST",
+        url: "/quiz/addGroup",
+        data: { groupName: groupName },
+        success: function (response) {
+            console.log("OK: added group");
+            // do something with the response, e.g. show a success message
+        },
+        error: function (xhr, status, error) {
+            console.log("BAD: NOT added group");
+            // handle the error, e.g. show an error message
+        }
+    });
+}
+
+
+function addQuestionToGroup(groupName, questionType, options, marks) {
+    var data = {
+        groupName: groupName,
+        questionType: questionType,
+        options: options,
+        marks: marks
+    };
+    $.ajax({
+        type: "POST",
+        url: "/quiz/addQuestionToGroup",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            console.log("OK: added question to group");
+            // do something with the response, e.g. show a success message
+        },
+        error: function (xhr, status, error) {
+            console.log("BAD: NOT added question to group");
+            // handle the error, e.g. show an error message
+        }
+    });
+}
+
+
+function deleteQuestionFromGroup(groupName, questionId) {
+    $.ajax({
+        type: "DELETE",
+        url: "/quiz/deleteQuestionFromGroup",
+        data: { groupName: groupName, questionId: questionId },
+        success: function (response) {
+            console.log("OK: deleted question from group");
+            // do something with the response, e.g. show a success message
+        },
+        error: function (xhr, status, error) {
+            console.log("BAD: NOT deleted question from group");
+            // handle the error, e.g. show an error message
+        }
+    });
+}
