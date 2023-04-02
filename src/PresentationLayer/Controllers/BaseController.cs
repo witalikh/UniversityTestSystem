c0a5988ce33b2b63ffbf78125e3b519665e124d1
User <vitalii.petliukh@outlook.com>
2023-04-02 21:23:27 +0200

    Some endpoints adjustment

---
+++ src/PresentationLayer/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿namespace PresentationLayer.Controllers
+{
+    using DataAccessLayer;
+    using DataAccessLayer.Enums;
+    using DataAccessLayer.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+        protected readonly UserManager<UserEntity> _userManager;
+
+        public BaseController(UserManager<UserEntity> manager)
+        {
+            this._userManager = manager;
+        }
+
+        protected string? GetUserId()
+        {
+            return this._userManager.GetUserId(this.User);
+        }
+
+        protected async Task<UserEntity?> GetUser()
+        {
+            return await this._userManager.GetUserAsync(this.User);
+        }
+
+        protected MembershipRole? GetUserRoleBelongingToClassroom(ClassroomEntity classroomEntity, string UserId)
+        {
+            if (classroomEntity.OwnerId == UserId)
+            {
+                return MembershipRole.Creator;
+            }
+
+            if (classroomEntity.Members.Any(m => m.UserEntityId == UserId))
+            {
+                return MembershipRole.Student;
+            }
+            return null;
+        }
+    }
+}
