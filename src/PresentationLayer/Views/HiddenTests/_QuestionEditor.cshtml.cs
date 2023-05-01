c1faf727e8543d8f26b68a00a613446833047a0b
User <olha.pastushuk@lnu.edu.ua>
2023-05-01 19:33:34 +0200

    Components to end

---
+++ src/PresentationLayer/Views/HiddenTests/_QuestionEditor.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PresentationLayer.Views.Tests
+{
+    public class _QuestionEditorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
