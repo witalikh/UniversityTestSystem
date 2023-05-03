cb280ee8776ea29b8a6721dfe76e26e9b19dbe00
User <olha.pastushuk@lnu.edu.ua>
2023-05-03 18:32:23 +0200

    Forgot other identity files

---
+++ src/PresentationLayer/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -0,0 +1,127 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace PresentationLayer.Areas.Identity.Pages.Account
+{
+#nullable disable
+
+    using System.ComponentModel.DataAnnotations;
+    using DataAccessLayer.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class LoginWith2faModel : PageModel
+    {
+        private readonly SignInManager<UserEntity> _signInManager;
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly ILogger<LoginWith2faModel> _logger;
+
+        public LoginWith2faModel(
+            SignInManager<UserEntity> signInManager,
+            UserManager<UserEntity> userManager,
+            ILogger<LoginWith2faModel> logger)
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+            this._logger = logger;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public bool RememberMe { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Text)]
+            [Display(Name = "Authenticator code")]
+            public string TwoFactorCode { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Display(Name = "Remember this machine")]
+            public bool RememberMachine { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync(bool rememberMe, string returnUrl = null)
+        {
+            // Ensure the userEntity has gone through the username & password screen first
+            var user = await this._signInManager.GetTwoFactorAuthenticationUserAsync();
+
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication userEntity.");
+            }
+
+            this.ReturnUrl = returnUrl;
+            this.RememberMe = rememberMe;
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(bool rememberMe, string returnUrl = null)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            returnUrl = returnUrl ?? this.Url.Content("~/");
+
+            var user = await this._signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication userEntity.");
+            }
+
+            var authenticatorCode = this.Input.TwoFactorCode.Replace(" ", string.Empty).Replace("-", string.Empty);
+
+            var result = await this._signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememberMe, this.Input.RememberMachine);
+
+            var userId = await this._userManager.GetUserIdAsync(user);
+
+            if (result.Succeeded)
+            {
+                this._logger.LogInformation("UserEntity with ID '{UserId}' logged in with 2fa.", user.Id);
+                return this.LocalRedirect(returnUrl);
+            }
+            else if (result.IsLockedOut)
+            {
+                this._logger.LogWarning("UserEntity with ID '{UserId}' account locked out.", user.Id);
+                return this.RedirectToPage("./Lockout");
+            }
+            else
+            {
+                this._logger.LogWarning("Invalid authenticator code entered for userEntity with ID '{UserId}'.", user.Id);
+                this.ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+                return this.Page();
+            }
+        }
+    }
+}
---
+++ src/PresentationLayer/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
@@ -0,0 +1,110 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace PresentationLayer.Areas.Identity.Pages.Account
+{
+#nullable disable
+
+    using System.ComponentModel.DataAnnotations;
+    using DataAccessLayer.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class LoginWithRecoveryCodeModel : PageModel
+    {
+        private readonly SignInManager<UserEntity> _signInManager;
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly ILogger<LoginWithRecoveryCodeModel> _logger;
+
+        public LoginWithRecoveryCodeModel(
+            SignInManager<UserEntity> signInManager,
+            UserManager<UserEntity> userManager,
+            ILogger<LoginWithRecoveryCodeModel> logger)
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+            this._logger = logger;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [BindProperty]
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Recovery Code")]
+            public string RecoveryCode { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+        {
+            // Ensure the userEntity has gone through the username & password screen first
+            var user = await this._signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication userEntity.");
+            }
+
+            this.ReturnUrl = returnUrl;
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            var user = await this._signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null)
+            {
+                throw new InvalidOperationException($"Unable to load two-factor authentication userEntity.");
+            }
+
+            var recoveryCode = this.Input.RecoveryCode.Replace(" ", string.Empty);
+
+            var result = await this._signInManager.TwoFactorRecoveryCodeSignInAsync(recoveryCode);
+
+            var userId = await this._userManager.GetUserIdAsync(user);
+
+            if (result.Succeeded)
+            {
+                this._logger.LogInformation("UserEntity with ID '{UserId}' logged in with a recovery code.", user.Id);
+                return this.LocalRedirect(returnUrl ?? this.Url.Content("~/"));
+            }
+            if (result.IsLockedOut)
+            {
+                this._logger.LogWarning("UserEntity account locked out.");
+                return this.RedirectToPage("./Lockout");
+            }
+            else
+            {
+                this._logger.LogWarning("Invalid recovery code entered for userEntity with ID '{UserId}' ", user.Id);
+                this.ModelState.AddModelError(string.Empty, "Invalid recovery code entered.");
+                return this.Page();
+            }
+        }
+    }
+}
